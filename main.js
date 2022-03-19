8283d2c4fa210444737e0f03a7f8b54d57824c75
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-03-19 11:05:52 -0500

    week2 NSS

---
+++ main.js
@@ -0,0 +1,39 @@
+const packGear = () => {
+    console.log("Pack water, fire starting, tent, sleep system, knife, etc.")
+}
+
+const findCampsite = () => {
+    console.log("Check for level ground and look for widowmakers.")
+}
+
+const gatherFirewood = () => {
+    console.log("Gather lots of firewood; especially if it's cold")
+}
+
+const startFire = () => {
+    console.log("Make sure to get some good coals going, so restarting a fire is easy if it goes out")
+}
+
+const setUpTent = () => {
+    console.log("Assemble the tent!")
+}
+
+const setUpSleepSystem = () => {
+    console.log("Air up your sleep pad and get sleeping bag laid out.")
+}
+
+const gatherMoreFirewood = () => {
+    console.log("Time for lots more firewood.")
+}
+
+const hangBag = () => {
+    console.log("Hang your bag so critters and animals can't get to it.")
+}
+packGear()
+findCampsite()
+gatherFirewood()
+startFire()
+setUpTent()
+setUpSleepSystem()
+gatherMoreFirewood()
+hangBag()
